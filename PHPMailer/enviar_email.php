153b04ac02f4d74735b1372f213d0dfe5bd20d5f
whellingtonpaes <whellington@gmail.com>
2018-07-09 00:19:59 -0300

    update

---
+++ PHPMailer/enviar_email.php
@@ -0,0 +1,42 @@
+<?php
+$name = $_POST['name'];
+//pega os dados que foi digitado no ID name.
+
+$email = $_POST['email'];
+//pega os dados que foi digitado no ID email.
+
+$subject = $_POST['subject'];
+//pega os dados que foi digitado no ID sebject.
+
+$message = $_POST['message'];
+//pega os dados que foi digitado no ID message.
+
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+/*abaixo contém os dados que serão enviados para o email
+cadastrado para receber o formulário*/
+
+$corpo = "Formulário enviado\n";
+$corpo .= "Nome: " . $name . "\n";
+$corpo .= "Email: " . $email . "\n";
+$corpo .= "Comentários: " . $message . "\n";
+
+$email_to = 'whellington@gmail.com';
+//não esqueça de substituir este email pelo seu.
+
+$status = mail($email_to, $subject, $corpo, $headers);
+//enviando o email.
+
+if ($status) {
+  echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+  
+//mensagem de form enviado com sucesso.
+
+} else {
+  echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+  
+//mensagem de erro no envio. 
+
+}
+?>
